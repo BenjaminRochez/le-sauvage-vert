32279f83b4418b0fc34e6d47d23b701bbc305cee
Benjamin Rochez <rochez.benjamin@gmail.com>
2018-12-12 16:43:04 +0100

    Update model & added category

--- website/sauvage/views.py
+++ website/sauvage/views.py
@@ -3,10 +3,10 @@ from .models import Picture
 
 # Create your views here.
 def index(request):
-    deboisements = Picture.objects.filter(category=1)
-    abattages = Picture.objects.filter(category = 2)
-    elagages = Picture.objects.filter(category = 3)
-    grues = Picture.objects.filter(category = 4)
-    rognages = Picture.objects.filter(category = 5)
-    equipements = Picture.objects.filter(category = 6)
+    deboisements = Picture.objects.filter(category_id= 2)
+    abattages = Picture.objects.filter(category_id = 1)
+    elagages = Picture.objects.filter(category_id = 3)
+    grues = Picture.objects.filter(category_id = 4)
+    rognages = Picture.objects.filter(category_id = 5)
+    equipements = Picture.objects.filter(category_id = 6)
     return render(request, 'sauvage/index.html', {'abattages':abattages, 'deboisements' : deboisements, 'elagages': elagages, 'grues': grues, 'rognages': rognages, 'equipements': equipements})
