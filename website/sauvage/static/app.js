680223774c08036f667775e380eb150b9364c54c
Benjamin Rochez <rochez.benjamin@gmail.com>
2018-12-16 18:33:43 +0100

    Added pace.js + scrollto + contact

--- website/sauvage/static/app.js
+++ website/sauvage/static/app.js
@@ -6,4 +6,22 @@ $(document).ready(function(){
           items: 2
       }
   );
+
+  Pace.on("done", function(){
+        setTimeout(function () {
+            $('.body').addClass('pace-delay');
+            console.log("done");
+        }, 200);
+    });
+
+  $('.scrollTo').click(function(){
+        //$('.nav__links').removeClass('is-open');
+
+        var the_id = $(this).attr("href");
+        $('html, body').animate({
+            scrollTop:$(the_id).offset().top
+        }, 'slow');
+        return false;
+    });
+
 });
